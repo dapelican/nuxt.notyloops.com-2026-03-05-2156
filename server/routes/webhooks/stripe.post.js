6c7bc2166791b569331a03e82c8e365c449c6bab
Dimitri <dalamkan@nomination.fr>
2026-05-11 20:59:34 +0200

    Add logs for debugging stripe webhook

--- server/routes/webhooks/stripe.post.js
+++ server/routes/webhooks/stripe.post.js
@@ -35,7 +35,7 @@ const PREMIUM_ACCESS_PAYMENT_LINK_ID_LIST = [
 ];
 
 const finish_checkout_session_completed_processing = async ({
-  customer_id,
+  user_id,
   payment_link_id,
   amount_paid,
 }) => {
@@ -43,10 +43,12 @@ const finish_checkout_session_completed_processing = async ({
     rows: user_list,
   } = await executeSQLQuery(
     'SELECT * FROM users WHERE id = $1',
-    [customer_id]
+    [user_id]
   );
 
   const user = user_list.at(0);
+
+  console.log('!!!!!!!user,', JSON.stringify(user));
 
   const new_premium_status_expiration_date = user.premium_status_expiration_date
     ? DateTime.fromISO(user.premium_status_expiration_date)
@@ -54,15 +56,19 @@ const finish_checkout_session_completed_processing = async ({
         .toISO()
     : DateTime.utc().plus({ years: 1 }).toISO();
 
+  console.log('!!!!!!!new_premium_status_expiration_date,', new_premium_status_expiration_date);
+  console.log('!!!!!!!PREMIUM_ACCESS_PAYMENT_LINK_ID_LIST,', PREMIUM_ACCESS_PAYMENT_LINK_ID_LIST);
+  console.log('!!!!!!!payment_link_id,', payment_link_id);
+
   if (PREMIUM_ACCESS_PAYMENT_LINK_ID_LIST.includes(payment_link_id)) {
     await Promise.all([
       executeSQLQuery(
-        'UPDATE users SET premium_status_expiration_date = $1 WHERE id = $2',
-        [new_premium_status_expiration_date, customer_id]
+        'UPDATE users SET status = $1, premium_status_expiration_date = $2 WHERE id = $3',
+        [USER_STATUS_PREMIUM, new_premium_status_expiration_date, user_id]
       ),
       executeSQLQuery(
         'INSERT INTO payments (payment_type, user_id, price_in_cents) VALUES ($1, $2, $3)',
-        ['premium_access', customer_id, amount_paid]
+        ['premium_access', user_id, amount_paid]
       ),
     ]);
   } else {
@@ -82,17 +88,21 @@ const finish_checkout_session_completed_processing = async ({
     await executeSQLQuery(
       `INSERT INTO payments (payment_type, user_id, collection_id, price_in_cents)
       VALUES ($1, $2, $3, $4)`,
-      ['premium_notes', customer_id, collection.id, amount_paid]
+      ['premium_notes', user_id, collection.id, amount_paid]
     );
   }
 };
 
 export default defineEventHandler(async (event) => {
+  console.log('!!!!!!!event,', JSON.stringify(event));
   try {
     const {
       STRIPE_ENDPOINT_SECRET,
       STRIPE_SECRET_API_KEY,
     } = useRuntimeConfig();
+
+    console.log('!!!!!!!STRIPE_SECRET_API_KEY,', STRIPE_SECRET_API_KEY);
+    console.log('!!!!!!!STRIPE_ENDPOINT_SECRET,', STRIPE_ENDPOINT_SECRET);
 
     if (!STRIPE_SECRET_API_KEY || !STRIPE_ENDPOINT_SECRET) {
       setResponseStatus(event, HTTP_CODE_400_BAD_REQUEST);
@@ -104,6 +114,9 @@ export default defineEventHandler(async (event) => {
 
     const raw_body = await readRawBody(event, 'utf8');
     const stripe_signature = getHeader(event, 'stripe-signature');
+
+    console.log('!!!!!!!raw_body,', raw_body);
+    console.log('!!!!!!!stripe_signature,', stripe_signature);
 
     if (!stripe_signature || raw_body === undefined || raw_body === null) {
       setResponseStatus(event, HTTP_CODE_400_BAD_REQUEST);
@@ -131,6 +144,8 @@ export default defineEventHandler(async (event) => {
       };
     }
 
+    console.log('!!!!!!!stripe_event,', JSON.stringify(stripe_event));
+
     if (stripe_event.type === CHECKOUT_SESSION_COMPLETED) {
       const session_from_event = stripe_event.data.object;
       const session = await stripe_client.checkout.sessions.retrieve(
@@ -139,10 +154,15 @@ export default defineEventHandler(async (event) => {
 
       const payment_link_id = session.payment_link ?? null;
       const amount_paid = session.amount_total;
-      const customer_id = session.client_reference_id;
+      const user_id = session.client_reference_id;
+
+      console.log('!!!!!!!session,', JSON.stringify(session));
+      console.log('!!!!!!!payment_link_id,', payment_link_id);
+      console.log('!!!!!!!amount_paid,', amount_paid);
+      console.log('!!!!!!!user_id,', user_id);
 
       const post_ack_processing = finish_checkout_session_completed_processing({
-        customer_id,
+        user_id,
         payment_link_id,
         amount_paid,
       });
