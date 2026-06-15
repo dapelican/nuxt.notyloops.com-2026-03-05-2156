af8e5c7bd0545fae31d553e82eac1da5e07070d4
Dimitri <dalamkan@nomination.fr>
2026-06-15 09:48:24 +0200

    Fix canonical url

--- app/app.vue
+++ app/app.vue
@@ -2,12 +2,21 @@
 useSchema();
 
 const i18n_head = useLocaleHead();
+const request_url = useRequestURL();
+const route = useRoute();
 
 useHead(() => ({
   htmlAttrs: {
     ...i18n_head.value.htmlAttrs,
   },
-  link: [...(i18n_head.value.link || [])],
+  link: [
+    {
+      key: 'canonical',
+      rel: 'canonical',
+      href: `${request_url.origin}${route.path}`,
+    },
+    ...(i18n_head.value.link || []),
+  ],
   meta: [...(i18n_head.value.meta || [])],
 }));
 </script>
