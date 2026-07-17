1bf1cf10360f0e3d5c96e4a15a9084826df573d1
Dimitri <dalamkan@nomination.fr>
2026-07-17 11:06:03 +0200

    Update collections  upon deletion of tag

--- server/routes/tags/delete.delete.js
+++ server/routes/tags/delete.delete.js
@@ -58,10 +58,57 @@ export default defineEventHandler(async (event) => {
 
     const final_tag_id_list = tag_list.map((tag) => tag.id);
 
-    await executeSQLQuery(
-      'DELETE FROM tags WHERE id = ANY($1::uuid[])',
-      [final_tag_id_list]
+    // Select the collection_id_list that belongs to the user and that has in the column
+    // tag_id_list_to_include or tag_id_list_to_exclude any of the tag
+    const {
+      rows: collection_list,
+    } = await executeSQLQuery(
+      `SELECT id
+       FROM collections
+       WHERE user_id = $1
+         AND (
+           (
+             tag_id_list_to_include IS NOT NULL
+             AND EXISTS (
+               SELECT 1
+               FROM jsonb_array_elements_text(tag_id_list_to_include) AS elem
+               WHERE elem::uuid = ANY($2::uuid[])
+             )
+           )
+           OR (
+             tag_id_list_to_exclude IS NOT NULL
+             AND EXISTS (
+               SELECT 1
+               FROM jsonb_array_elements_text(tag_id_list_to_exclude) AS elem
+               WHERE elem::uuid = ANY($2::uuid[])
+             )
+           )
+         )`,
+      [user.id, final_tag_id_list]
     );
+
+    const collection_id_list = collection_list.map((collection) => collection.id);
+
+    // Update the collection_id_list to remove the tag_id_list from the columns
+    // tag_id_list_to_include and tag_id_list_to_exclude
+    if (collection_id_list.length > 0) {
+      await executeSQLQuery(
+        `UPDATE collections
+         SET
+           tag_id_list_to_include = (
+             SELECT COALESCE(jsonb_agg(to_jsonb(elem)), '[]'::jsonb)
+             FROM jsonb_array_elements_text(COALESCE(tag_id_list_to_include, '[]'::jsonb)) AS elem
+             WHERE elem::uuid <> ALL($2::uuid[])
+           ),
+           tag_id_list_to_exclude = (
+             SELECT COALESCE(jsonb_agg(to_jsonb(elem)), '[]'::jsonb)
+             FROM jsonb_array_elements_text(COALESCE(tag_id_list_to_exclude, '[]'::jsonb)) AS elem
+             WHERE elem::uuid <> ALL($2::uuid[])
+           )
+         WHERE id = ANY($1::uuid[])`,
+        [collection_id_list, final_tag_id_list]
+      );
+    }
 
     setResponseStatus(event, HTTP_CODE_204_NO_CONTENT);
 
