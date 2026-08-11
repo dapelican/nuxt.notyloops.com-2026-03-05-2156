4d4dbbdcd29936c34814e9147b4db4b7da4504bf
Dimitri <dalamkan@nomination.fr>
2026-08-11 19:02:11 +0200

    Update annales

--- app/pages/annales/cpge/pc/physique-chimie.vue
+++ app/pages/annales/cpge/pc/physique-chimie.vue
@@ -72,6 +72,244 @@ useGoogleTag();
         <tbody>
           <tr>
             <td>
+              CCINP
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccinp-sujet-physique.pdf"
+                target="_blank"
+              >
+                Sujet physique
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccinp-rapport-physique.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccinp-sujet-chimie.pdf"
+                target="_blank"
+              >
+                Sujet chimie
+              </ULink>
+              <!-- |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccinp-rapport-chimie.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink> -->
+            </td>
+          </tr>
+          <tr>
+            <td>
+              CCINP / e3a-Polytech
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccinp-e3a-polytech-sujet-modelisation.pdf"
+                target="_blank"
+              >
+                Sujet modélisation
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccinp-e3a-polytech-rapport-modelisation.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              CCMP
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccmp-sujet-physique-1.pdf"
+                target="_blank"
+              >
+                Sujet physique 1
+              </ULink>
+              <!-- |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccmp-rapport-physique-1.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink> -->
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccmp-sujet-physique-2.pdf"
+                target="_blank"
+              >
+                Sujet physique 2
+              </ULink>
+              <!-- |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccmp-rapport-physique-2.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink> -->
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccmp-sujet-chimie.pdf"
+                target="_blank"
+              >
+                Sujet chimie
+              </ULink>
+              <!-- |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccmp-rapport-chimie.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink> -->
+            </td>
+          </tr>
+          <tr>
+            <td>
+              CCS
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccs-sujet-phyqique-1.pdf"
+                target="_blank"
+              >
+                Sujet physique 1
+              </ULink>
+              <!-- |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccs-rapport-phyqique-1.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink> -->
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccs-sujet-phyqique-2.pdf"
+                target="_blank"
+              >
+                Sujet physique 2
+              </ULink>
+              <!-- |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccs-rapport-phyqique-2.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink> -->
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccs-sujet-chimie.pdf"
+                target="_blank"
+              >
+                Sujet chimie
+              </ULink>
+              <!-- |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ccs-rapport-chimie.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink> -->
+            </td>
+          </tr>
+          <tr>
+            <td>
+              e3a-Polytech
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-e3a-polytech-sujet.pdf"
+                target="_blank"
+              >
+                Sujet
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-e3a-polytech-rapport.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-e3a-polytech-corrige.pdf"
+                target="_blank"
+              >
+                Corrigé
+              </ULink>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              ENS
+            </td>
+            <td>
+              Sujet physique C (pas encore disponible)
+              <!-- <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ens-sujet-physique-c.pdf"
+                target="_blank"
+              >
+                Sujet physique C
+              </ULink> -->
+              <!-- |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ens-rapport-physique-c.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink> -->
+              <br>
+              Sujet chimie B (pas encore disponible)
+              <!-- <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ens-sujet-chimie-b.pdf"
+                target="_blank"
+              >
+                Sujet chimie B
+              </ULink> -->
+              <!-- |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2026-pc-ens-rapport-chimie-b.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink> -->
+            </td>
+          </tr>
+          <tr>
+            <td>
               X / ESPCI
             </td>
             <td>
@@ -153,6 +391,242 @@ useGoogleTag();
         <tbody>
           <tr>
             <td>
+              CCINP
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccinp-sujet-physique.pdf"
+                target="_blank"
+              >
+                Sujet physique
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccinp-rapport-physique.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccinp-sujet-chimie.pdf"
+                target="_blank"
+              >
+                Sujet chimie
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccinp-rapport-chimie.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              CCINP / e3a-Polytech
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccinp-e3a-polytech-sujet-modelisation.pdf"
+                target="_blank"
+              >
+                Sujet modélisation
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccinp-e3a-polytech-rapport-modelisation.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              CCMP
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccmp-sujet-physique-1.pdf"
+                target="_blank"
+              >
+                Sujet physique 1
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccmp-rapport-physique-1.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccmp-sujet-physique-2.pdf"
+                target="_blank"
+              >
+                Sujet physique 2
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccmp-rapport-physique-2.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccmp-sujet-chimie.pdf"
+                target="_blank"
+              >
+                Sujet chimie
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccmp-rapport-chimie.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              CCS
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccs-sujet-phyqique-1.pdf"
+                target="_blank"
+              >
+                Sujet physique 1
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccs-rapport-phyqique-1.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccs-sujet-phyqique-2.pdf"
+                target="_blank"
+              >
+                Sujet physique 2
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccs-rapport-phyqique-2.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccs-sujet-chimie.pdf"
+                target="_blank"
+              >
+                Sujet chimie
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ccs-rapport-chimie.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              e3a-Polytech
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-e3a-polytech-sujet.pdf"
+                target="_blank"
+              >
+                Sujet
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-e3a-polytech-rapport.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-e3a-polytech-corrige.pdf"
+                target="_blank"
+              >
+                Corrigé
+              </ULink>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              ENS
+            </td>
+            <td>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ens-sujet-physique-c.pdf"
+                target="_blank"
+              >
+                Sujet physique C
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ens-rapport-physique-c.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+              <br>
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ens-sujet-chimie-b.pdf"
+                target="_blank"
+              >
+                Sujet chimie B
+              </ULink>
+              |
+              <ULink
+                class="text-primary"
+                to="https://annales.notyloops.com/cpge-physique-chimie-2025-pc-ens-rapport-chimie-b.pdf"
+                target="_blank"
+              >
+                Rapport
+              </ULink>
+            </td>
+          </tr>
+          <tr>
+            <td>
               X / ESPCI
             </td>
             <td>
@@ -163,6 +637,7 @@ useGoogleTag();
               >
                 Sujet physique A
               </ULink>
+              | Rapport non disponible
               <!-- |
               <ULink
                 class="text-primary"
